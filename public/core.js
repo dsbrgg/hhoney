fef7c58c71a79b7d867b75ee0dccda4d6dc3db21
opuzzz <dsbrgg@gmail.com>
2018-03-16 11:05:24 +0000

    General configuration done

---
+++ public/core.js
@@ -0,0 +1,24 @@
+// MODULE
+var core = angular.module('core', [ 'ngRoute' ]);
+
+core.config(function($routeProvider, $locationProvider) {
+	$locationProvider.hashPrefix('');
+
+	$routeProvider
+		.when('/', {
+			templateUrl : '/pages/construction.htm',
+			controller  : 'mainController'
+		});
+});
+
+// CONTROLLERS
+core.controller('mainController', ['$scope', '$filter', '$log',
+	function($scope, $filter, $log) {
+        $scope.generalInfo = {
+            contact : 'CONTACTO :',
+            email   : 'hawaiianhoney@bol.com.br',
+            number  : '(351) 912 340 636',
+            sign    : 'Abelhinha Inc. ©'
+        };
+	}
+]);
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
-const hbs 	  = require('hbs');
 
 const port = process.env.PORT || 3000;
 var app = express();
 
-app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
-
-
+ 
+app.get('*', (req, res) => res.sendFile(__dirname+'/public/index.htm'));
 
 // SERVER LISTEN
 app.listen(port, () => {
